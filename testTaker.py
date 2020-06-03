245c7d91272d9f7699b4a1645ebb1b8dc7d025eb
Saahil Parikh <saahilparikh@berkeley.edu>
2020-06-02 19:20:18 -0700

    Dev Has a Tester

--- testTaker.py
+++ testTaker.py
@@ -8,6 +8,11 @@ def singleTime(CPM):
     result = post(CPM, CPM+wrong, wrong, CPM//6, ip, CPM+wrong+CPM//6, CPM) 
     print("Attempted to send a request with a cpm of " + str(CPM) + " and ip of " + ip + ". Result: " + result.text)
 
+def singleTester(CPM):
+    ip = '0'
+    result = post(CPM, 0, 0, 0, ip, 0, 0)
+    print("Attempted to send a request with a cpm of " + str(CPM) + " and ip of " + ip + ". Result: " + result.text)
+
 def multipleTimes(CPM, numTimes):
     for i in range(numTimes):
         adjCPM = CPM + random.randint(-40, 40)
